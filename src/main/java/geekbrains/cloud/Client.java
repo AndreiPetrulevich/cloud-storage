54ed3530d018d546b6ee6c225b08a32949b1ae26
Ond Re <aapetrulevich@icloud.com>
2022-01-26 10:50:51 +0300

    add classes: Client, Chat, ChatController

---
+++ src/main/java/geekbrains/cloud/Client.java
@@ -0,0 +1,96 @@
+package geekbrains.cloud;
+
+import java.io.*;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.nio.file.Files;
+import java.util.List;
+import java.util.function.Function;
+
+public class Client {
+    Socket socket;
+    DataInputStream is;
+    DataOutputStream os;
+    Function<String, Void> onMessageReceived;
+
+    public Client() {
+        try {
+            this.socket = new Socket("localhost", 8190);
+            is = new DataInputStream(socket.getInputStream());
+            os = new DataOutputStream(socket.getOutputStream());
+
+            start();
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void start() {
+        try {
+            var readThread = new Thread(() -> {
+                while (true) {
+                    try {
+                        int rawValue = is.readInt();
+                        switch (MessageType.fromRawValue(rawValue)) {
+                            case TEXT -> {
+                                String msg = is.readUTF();
+                                if (onMessageReceived != null) {
+                                    onMessageReceived.apply(msg);
+                                }
+                            }
+                            case FILE -> {
+                                System.out.println("FILE message type not yet supported");
+                            }
+                        }
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            });
+            readThread.setDaemon(true);
+            readThread.start();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void sendMessage(String msg) {
+        try {
+            os.writeInt(MessageType.TEXT.getRawValue());
+            os.writeUTF(msg);
+            os.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void uploadFile (List<File> files) {
+        if (files.size() == 0) return;
+
+        try {
+            for(File file : files) {
+                String name = file.getName();
+                if(name.length() > MessageType.MAX_FILENAME_LENGTH) {
+                    throw new Exception("Too long filename");
+                }
+                os.writeInt(MessageType.FILE.getRawValue());
+                os.writeUTF(name);
+
+                FileInputStream fileInput = new FileInputStream(file);
+
+                if (Files.size(file.toPath()) < Integer.MAX_VALUE) {
+                    os.writeLong(Files.size(file.toPath()));
+                    byte[] buffer = new byte[8190];
+                    while (fileInput.read(buffer) != -1) {
+                        os.write(buffer);
+                    }
+                }
+
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
--- src/main/java/geekbrains/cloud/Handler.java
+++ src/main/java/geekbrains/cloud/Handler.java
@@ -1,11 +1,9 @@
 package geekbrains.cloud;
 
-import java.awt.*;
 import java.io.*;
 import java.net.Socket;
 import java.nio.charset.Charset;
 import java.util.UUID;
-import java.util.logging.Logger;
 
 public class Handler implements Runnable, Closeable {
     private final Socket socket;
@@ -26,29 +24,27 @@ public class Handler implements Runnable, Closeable {
                         System.out.println("Received text message:" + msg);
                         os.writeInt(MessageType.TEXT.getRawValue());
                         os.writeUTF(msg);
+                        os.flush();
                     }
                     case FILE -> {
-                        int filenameLength = is.readInt();
-                        byte[] filenameBytes = is.readNBytes(filenameLength);
-                        String filename = new String(filenameBytes, Charset.forName("utf-8"));
+                        String filename = is.readUTF();
+                        System.out.println(filename);
                         long totalBytes = is.readLong();
 
                         UUID fileUUID = UUID.randomUUID();
 
                         FileOutputStream fileInput = new FileOutputStream(fileUUID.toString());
 
-                        byte[] buffer;
                         long readBytes = 0;
-                        do {
-                            buffer = is.readNBytes(8190);
+                        while (readBytes < totalBytes) {
+                            byte[] buffer = is.readNBytes(8190);
                             readBytes += buffer.length;
                             if(buffer.length > 0) {
                                 fileInput.write(buffer);
                             }
-                            if(readBytes == totalBytes) {
-                                break;
-                            }
-                        } while (buffer.length > 0);
+                        }
+                        os.writeInt(MessageType.TEXT.getRawValue());
+                        os.writeUTF("File sent: " + filename);
                     }
                 }
             }
